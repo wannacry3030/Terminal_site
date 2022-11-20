b29a0d23f1fcaf823af4856fe6263d938fcbf5fa
wannacry3030 <wannacry.bat@proton.me>
2022-11-20 15:15:33 -0300

    first commit

---
+++ commands.js
@@ -0,0 +1,103 @@
+var youtube = "https://www.youtube.com/fknight/";
+var twitter = "https://www.twitter.com/forrestpknight/";
+var password = "fkcodes";
+var linkedin = "https://www.linkedin.com/in/forrestpknight/";
+var instagram = "https://www.instagram.com/forrestpknight/";
+var github = "https://github.com/forrestknight/";
+var email = "mailto:forrest@fkcodes.com";
+
+whois = [
+  "<br>",
+  "Hey, I'm Forrest!👋",
+  "I'm a software developer and content creator, who builds engaging websites like this one",
+  "and makes YouTube videos about computer science & software engineering.",
+  "After graduating with a Bachelor's in Computer Science, I worked professionally",
+  "as a software engineer building enterprise web applications for Fortune 500 companies.",
+  "While doing all of that, I documentned my coding journey on YouTube - trying to enlighten",
+  "the next generation of developers and help them navigate the crazy world that is software",
+  "development & computer science.",
+  "Before I knew it, that online presence took on a life of its own, to the point where I knew",
+  "I needed to make the jump from software engineering to full time content creator, and it's",
+  "the best decision I ever made.",
+  "Now, I make videos about creating cool shit like this terminal website, and hosting my",
+  "podcast 'Decoded w/ Forrest Knight.' What most people don't know, and will only know",
+  "because they're reading this right now, is that I also run a creative & media agency.",
+  "We partner with clients to drive their business outcomes using modern marketing strategies.",
+  "<br>",
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>",
+];
+
+social = [
+  "<br>",
+  'youtube        <a href="' +
+    youtube +
+    '" target="_blank">youtube/fknight' +
+    "</a>",
+  'twitter        <a href="' +
+    twitter +
+    '" target="_blank">twitter/forrestpknight' +
+    "</a>",
+  'linkedin       <a href="' +
+    linkedin +
+    '" target="_blank">linkedin/forrestpknight' +
+    "</a>",
+  'instagram      <a href="' +
+    instagram +
+    '" target="_blank">instagram/forrestpknight' +
+    "</a>",
+  'github         <a href="' +
+    github +
+    '" target="_blank">github/forrestknight' +
+    "</a>",
+  "<br>",
+];
+
+secret = [
+  "<br>",
+  '<span class="command">sudo</span>           Only use if you\'re admin',
+  "<br>",
+];
+
+projects = [
+  "<br>",
+  "Still curating... most projects are offline, on GitHub, or confidential.",
+  "<br>",
+];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Forrest?',
+  '<span class="command">whoami</span>         Who are you?',
+  '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  '<span class="index">f.nrirdesign @ Not A Corporation. Open  source</span>',
+  " .",
+  "    ▄████████ ███▄▄▄▄      ▄████████  ▄█     ▄████████ ████████▄     ▄████████    ▄████████  ▄█     ▄██████▄  ███▄▄▄▄",
+  "   ███    ███ ███▀▀▀██▄   ███    ███ ███    ███    ███ ███   ▀███   ███    ███   ███    ███ ███    ███    ███ ███▀▀▀██▄",
+  "   ███    █▀  ███   ███   ███    ███ ███▌   ███    ███ ███    ███   ███    █▀    ███    █▀  ███▌   ███    █▀  ███   ███",
+  "  ▄███▄▄▄     ███   ███  ▄███▄▄▄▄██▀ ███▌  ▄███▄▄▄▄██▀ ███    ███  ▄███▄▄▄       ███        ███▌  ▄███        ███   ███",
+  " .▀▀███▀▀▀     ███   ███ ▀▀███▀▀▀▀▀   ███▌ ▀▀███▀▀▀▀▀   ███    ███ ▀▀███▀▀▀     ▀███████████ ███▌ ▀▀███ ████▄  ███   ███",
+  "   ███        ███   ███ ▀███████████ ███  ▀███████████ ███    ███   ███    █▄           ███ ███    ███    ███ ███   ███",
+  "   ███        ███   ███   ███    ███ ███    ███    ███ ███   ▄███   ███    ███    ▄█    ███ ███    ███    ███ ███   ███",
+  "   ███         ▀█   █▀    ███    ███ █▀     ███    ███ ████████▀    ██████████  ▄████████▀  █▀     ████████▀   ▀█   █▀",
+  "                          ███    ███        ███    ███",
+  ".",
+  '<span class="color2">Bem vindo ao meu terminal interativo.</span>',
+  '<span class="color2">Para uma lista de comandos disponiveis, digite</span> <span class="command">\'ajuda\'</span><span class="color2">.</span>',
+];
