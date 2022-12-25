8adaea4337c415623f50998f9dcb53f8a507a287
wannacry3030 <wannacry.bat@proton.me>
2022-12-25 19:32:00 -0300

    modified:   commands.js

--- caret.js
+++ caret.js
@@ -1,38 +1,44 @@
+//retorna uma referência para um elemento no documento com o elid especificado como seu id.
 function $(elid) {
-    return document.getElementById(elid);
-  }
-  
+  return document.getElementById(elid);
+}
+
 var cursor;
 window.onload = init;
 
+//inicializa o script definindo a propriedade de estilo left de um elemento com o id "cursor" como "0px"
 function init() {
   cursor = $("cursor");
   cursor.style.left = "0px";
 }
 
+// substitui os caracteres de uma nova linha (\n) na string txt por elementos <br>.
 function nl2br(txt) {
-  return txt.replace(/\n/g, '');
+  return txt.replace(/\n/g, "");
 }
 
+// define a propriedade innerHTML de um elemento com o id "typer" com o valor de 'from.value', com os caracteres da nova linha substituídos por elementos <br>.
 function typeIt(from, e) {
   e = e || window.event;
   var w = $("typer");
   var tw = from.value;
-  if (!pw){
+  if (!pw) {
     w.innerHTML = nl2br(tw);
   }
 }
 
+// move um elemento com o id "cursor" para a esquerda ou para a direita no documento com base nas setas pressionadas pelo usuário. O parâmetro count especifica o número de vezes que o elemento deve ser movido.
 function moveIt(count, e) {
   e = e || window.event;
   var keycode = e.keyCode || e.which;
-  if (keycode == 37 && parseInt(cursor.style.left) >= (0 - ((count - 1) * 10))) {
+  if (keycode == 37 && parseInt(cursor.style.left) >= 0 - (count - 1) * 10) {
     cursor.style.left = parseInt(cursor.style.left) - 10 + "px";
-  } else if (keycode == 39 && (parseInt(cursor.style.left) + 10) <= 0) {
+  } else if (keycode == 39 && parseInt(cursor.style.left) + 10 <= 0) {
     cursor.style.left = parseInt(cursor.style.left) + 10 + "px";
   }
 }
 
+// registra a string txt na console.
 function alert(txt) {
   console.log(txt);
 }
--- commands.js
+++ commands.js
@@ -8,7 +8,14 @@ var email = "mailto:f.nrirdesign@gmail.com";
 
 whois = [
   "<br>",
-   
+  "Desde pequeno descobri minha paixão pela tecnologia, aos 6 anos de idades já desmontava ",
+  "os meus brinquedos eletrônicos para tentar entender como eles funcionavam, aos 13 já",
+  "estava familiarizado com a internet, já entendia tecnologia como HTML e CSS, além de ",
+  "tecnologias voltadas para o design e video, como Photoshop e Sony vegas, hoje já ",
+  "entendo linguagens como javascript e python3, além de dominar ferramentas modernas de ",
+  "web-design",
+
+  "nos últimos 9 anos prestei serviços para UNB, CAIXA ECONÔMICA e atualmente AMBEV.",
   "<br>",
 ];
 
